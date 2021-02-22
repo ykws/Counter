6b7fc5cadbd81fb33a0fafdfc5f48eddefd4e656
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-02-23 01:16:35 +0900

    :rocket: Create a new project

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by KAWASHIMA Yoshiyuki on 2021/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
